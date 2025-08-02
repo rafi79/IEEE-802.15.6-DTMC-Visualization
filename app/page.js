f2445325e178eb41dc9de269986a6d29373f507e
rafi79 <96584409+rafi79@users.noreply.github.com>
2025-08-02 20:40:10 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import DTMCVisualization from '../components/DTMCVisualization'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen py-8">
+      <DTMCVisualization />
+    </main>
+  )
+}
